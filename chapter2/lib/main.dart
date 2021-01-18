138dd73370c2be314386810f15ed4a6f76781220
Vatsa0408 <you@example.com>
2021-01-18 16:44:59 +0100

    converted to stateful widget. Work fine

--- chapter2/lib/main.dart
+++ chapter2/lib/main.dart
@@ -4,16 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s youre favourite place?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('First Flutter App'),
-        ),
-        body: Text('Default Text'),
-      ),
+          appBar: AppBar(
+            title: Text('Basic Quiz App'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(questions[questionIndex]),
+              RaisedButton(
+                child: Text('Answer 01'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 02'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 03'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }
